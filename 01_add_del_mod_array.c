c92a4fbad854c4f88fd401729b0ab4c7811cd626
ayan-adak <101331393+ayan-adak@users.noreply.github.com>
2022-04-20 03:58:45 +0000

    added a new program

---
+++ 01_add_del_mod_array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+  int x=0,i,n,add;
+  int arr[x];
+  printf("Enter length of Array: ");
+  scanf("%d",&x);
+  for (i = 0; i < x; i++)
+    {
+      printf("Enter the Elements of the array");
+      scanf("%d",&n);
+    }
+  printf("")
+  }    
