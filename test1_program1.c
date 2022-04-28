92838242c94c3ad1f818d0d26a9a5aeef256e96b
ayan-adak <101331393+ayan-adak@users.noreply.github.com>
2022-04-28 15:20:44 +0000

    test 1 programs 1,2,3

---
+++ test1_program1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int i=0,j,ar=0,k,n,c;
+    scanf("%d %d",&n,&c);
+    int *a=malloc(sizeof(int)*n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(j=0;j<n;j++)
+    {
+        for(k=j+1;k<n;k++)
+        {
+            if((a[j]+a[k]) % c==0)
+            ar++;
+        }
+    }
+    printf("%d\n",ar);
+}
---
+++ test1_program2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,i,j,arr=0,t;
+    int stat[5]={0};
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&t);
+        stat[t-1]++;
+    }
+    for(j=0;j<5;j++)
+    {
+        if(stat[j] > stat[arr]) 
+        arr = j;
+    }
+    arr=arr+1;
+    printf("%d\n",arr);
+    return 0;
+}
