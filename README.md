a8ae4f800613ad25ab19127d3be0c65241dcd8d3
ayan-adak <101331393+ayan-adak@users.noreply.github.com>
2022-05-29 05:03:22 +0000

    added test2 program7

--- README.md
+++ README.md
@@ -32,4 +32,3 @@ Readme File For Easy Access to Programs
 [Test 1 Program 4 VPL](https://github.com/ayan-adak/19ECB132/blob/main/test1_program4.c)
 
 [Test 1 Program 5 VPL](https://github.com/ayan-adak/19ECB132/blob/main/test1_program5.c)
-
---
+++ test2_program7.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int items;
+    struct node *left;
+    struct node *right;
+};
+struct node* insert(struct node* root,int items) 
+{
+    if(root==NULL) 
+    {
+        struct node* node=(struct node*)malloc(sizeof(struct node));
+        node->items=items;
+        node->left=NULL;
+        node->right=NULL;
+        return node;
+    } 
+    else 
+    {
+        struct node* cur;
+        if(items<=root->items) 
+        {
+            cur=insert(root->left,items);
+            root->left=cur;
+        } 
+        else 
+        {
+            cur=insert(root->right,items);
+            root->right=cur;
+        }
+        return root;
+    }
+}
+void pre(struct node *root) 
+{
+if(root!=NULL) 
+{
+    printf("%d ",root->items);
+    pre(root->left);
+    pre(root->right);
+}
+}
+int main() 
+{
+    struct node* root=NULL;
+    int t;
+    int d;
+    scanf("%d",&t);
+    while(t-->0) 
+    {
+        scanf("%d",&d);
+        root=insert(root,d);
+    }
+    pre(root);
+    return 0;
+}
