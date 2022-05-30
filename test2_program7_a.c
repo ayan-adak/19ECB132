745dc800987849224ed90c3e7c46d68539a62531
ayan-adak <101331393+ayan-adak@users.noreply.github.com>
2022-05-30 04:34:20 +0000

    added program

---
+++ test2_program7_a.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node 
+{
+    int item;
+    struct node *left;
+    struct node *right;
+};
+struct node* insert(struct node* root, int item) 
+{
+	if(root == NULL) 
+	{
+        struct node* node=(struct node*)malloc(sizeof(struct node));
+        node->item = item;
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+	} 
+	else 
+	{
+		struct node* c;
+		if(item<=root->item) 
+		{
+            c=insert(root->left,item);
+            root->left=c;
+		} else 
+		{
+            c=insert(root->right,item);
+            root->right=c;
+		}
+		return root;
+	}
+}
+int getHeight(struct node* root) 
+{
+    if (root==NULL) 
+    {
+        return -1;
+    }
+    else 
+    {
+        int left=getHeight(root->left);
+        int right=getHeight(root->right);
+        if (left>right) 
+            return left+1;
+        else  
+            return right+1;
+        
+    }
+}
+int main() 
+{
+    struct node* root = NULL;
+    int t;
+    int d;
+    scanf("%d", &t);
+    while(t-- > 0) 
+    {
+        scanf("%d",&d);
+        root=insert(root,d);
+    }
+	printf("%d",getHeight(root));
+    return 0;
+}
